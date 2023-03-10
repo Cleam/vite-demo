a44444cd75d055d564a76dee8bf993d423d743c5
lizhigao <lizhigao@021.com>
2023-03-10 11:44:46 +0800

    feat: add turbo

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { ConfigEnv, defineConfig, loadEnv, UserConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+// import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({ mode, command }: ConfigEnv): UserConfig => {
+  // const c = loadEnv(mode, resolve(__dirname));
+  // console.log("c :>> ", c);
+  const viteConfig: UserConfig = {
+    plugins: [vue()],
+  };
+
+  return viteConfig;
+});
